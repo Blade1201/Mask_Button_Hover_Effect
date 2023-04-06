4a155c57d59c90279ec6190a92f98c05671b11b5
Aaron <mol.aron@hotmail.com>
2023-04-06 20:40:03 +0200

    Added to portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="button-container-1">
+        <span className="mask">MASK1</span>
+        <button>MASK1</button>
+      </div>
+
+      <div className="button-container-2">
+        <span className="mask">MASK2</span>
+        <button>MASK2</button>
+      </div>
+
+      <div className="button-container-3">
+        <span className="mask">MASK3</span>
+        <button>MASK3</button>
+      </div>
+
     </div>
   );
 }
